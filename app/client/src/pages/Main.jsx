9fa63487bfd0e176dcfbf18ac5082679a57d109d
rjeantet <rose.jeantet@gmail.com>
2023-09-02 08:39:56 +0200

    update main and StartScreen

--- app/client/src/pages/Main.jsx
+++ app/client/src/pages/Main.jsx
@@ -1,9 +1,16 @@
-const Home = () => {
+import TestConnect from '../components/TestConnect';
+
+const Main = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Main</h1>
+      <ul>
+        <li>My own path</li>
+        <li>Catalog</li>
+      </ul>
+      <TestConnect />
     </div>
   );
 };
 
-export default Home;
+export default Main;
---
+++ app/client/src/pages/StartScreen.jsx
@@ -0,0 +1,21 @@
+const StartScreen = () => {
+  return (
+    <>
+      <h1>Talk aPalooza</h1>
+      <h2>StartScreen</h2>
+      <button>Get Started</button>
+      <button>I have an account</button>
+
+      <hr></hr>
+      <div>
+        (shortcuts):
+        <button>StartScreen</button>
+        <button>Login</button>
+        <button>Main (private)</button>
+        <button>Dashboard (private)</button>
+      </div>
+    </>
+  );
+};
+
+export default StartScreen;
