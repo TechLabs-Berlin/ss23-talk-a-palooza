951bece0d0abe2b9d3af11b255e15f61abd3cad6
rjeantet <rose.jeantet@gmail.com>
2023-09-06 07:43:21 +0200

    cleanup commented code app.js

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,59 +1,7 @@
 import { AuthWrapper } from './components/auth/AuthWrapper';
 
 const App = () => {
-  return (
-    <>
-      <AuthWrapper />
-    </>
-  );
-
-  // <Routes>
-  //   <Route index elements={<StartScreen />} />
-  //   <Route path='startscreen' element={<StartScreen />} />
-  //   <Route path='login' element={<Login />} />
-  //   <Route
-  //     path='main'
-  //     element={
-  //       <ProtectedRoute user={user}>
-  //         <Main />
-  //       </ProtectedRoute>
-  //     }
-  //   />
-  // </Routes>
-
-  // if (user) {
-  //   return (
-  //     <Routes>
-  //       <Route
-  //         path='/'
-  //         element={
-  //           user.children[0] ? <Navigate to='/main' /> : <InitialAssessment />
-  //         }
-  //       />
-  //       <Route
-  //         path='/login'
-  //         element={user.children[0] ? <Main /> : <InitialAssessment />}
-  //       />
-  //       <Route
-  //         path='/main'
-  //         element={user.children[0] ? <Main /> : <InitialAssessment />}
-  //       />
-  //       <Route
-  //         path='/dashboard'
-  //         element={user.children[0] ? <Dashboard /> : <InitialAssessment />}
-  //       />
-  //     </Routes>
-  //   );
-  // } else {
-  //   return (
-  //     <Routes>
-  //       <Route path='/' element={<StartScreen />} />
-  //       <Route path='/login' element={<Login />} />
-  //       <Route path='/main' element={<Main />} />
-  //       <Route path='/dashboard' element={<Dashboard />} />
-  //     </Routes>
-  //   );
-  // }
+  return <AuthWrapper />;
 };
 
 export default App;
