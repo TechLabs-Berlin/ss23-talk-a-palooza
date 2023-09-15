861e27d7dbb036aa5ca64a74ff8c94d1a0ee384b
rjeantet <rose.jeantet@gmail.com>
2023-09-15 17:32:31 +0200

    Final fixed before releasing Initial Assessment to main (left to do: frontEnd work & plug to API)

--- app/client/components/InitialAssessment/AddWords.jsx
+++ app/client/components/InitialAssessment/AddWords.jsx
@@ -1,5 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import AssessForm from './AssessForm';
 import AssessSuccess from './AssessSuccess';
 import { createVocab } from '../../services/vocabLogsService';
@@ -28,53 +27,15 @@ const AddWords = ({ child }) => {
       setIsAssessed(true);
       console.log('is assessed?', isAssessed);
     } else {
-      // Handle the case where values.words is not an array
       console.error('values.words is not an array:', values.words);
     }
   };
 
-  return (
-    <View style={styles.app}>
-      <Text style={styles.title}>{child.firstName} can say...</Text>
-      {isAssessed ? (
-        <AssessSuccess child={child} />
-      ) : (
-        <AssessForm child={child} onSubmit={handleWordsSubmit} />
-      )}
-    </View>
+  return isAssessed ? (
+    <AssessSuccess child={child} />
+  ) : (
+    <AssessForm child={child} onSubmit={handleWordsSubmit} />
   );
 };
 
 export default AddWords;
-
-const styles = StyleSheet.create({
-  app: {
-    marginHorizontal: 'auto',
-    maxWidth: 600,
-    maxHeight: 768,
-    padding: 20,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: '1.5rem',
-    marginTop: '1em',
-    textAlign: 'center',
-  },
-  text: {
-    lineHeight: '1.5em',
-    fontSize: '1.125rem',
-    marginVertical: '1em',
-    textAlign: 'center',
-  },
-  link: {
-    color: '#1977f2',
-  },
-  listitem: {
-    marginVertical: '0.5rem',
-  },
-  pageLink: {
-    fontSize: '1.25rem',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
