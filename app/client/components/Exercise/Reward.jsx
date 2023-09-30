f515fc337870c83e82862ff63ae50633448dea5d
rjeantet <rose.jeantet@gmail.com>
2023-09-30 17:06:22 +0200

    work on reward component

--- app/client/components/Exercise/Reward.jsx
+++ app/client/components/Exercise/Reward.jsx
@@ -1,19 +1,40 @@
-import { AuthData } from '../../services/AuthWrapper';
+import { ChildData } from '../../services/AuthWrapper';
 import { StyleSheet, View, Pressable, Text } from 'react-native';
-import { HomeButton, NextButton } from '../navigation/Buttons';
+import { Video, ResizeMode } from 'expo-av';
 
-const Reward = ({}) => {
-  const { authUser, child } = AuthData();
+import { HomeButton, NextButton } from '../navigation/Buttons';
+import { useState, useRef } from 'react';
+
+const Reward = () => {
+  const { child } = ChildData();
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
   //TODO: import results from Audio Exercise
   return (
     <>
       <View style={styles.container}>
         <View style={styles.row}>
-          <Text>Reward Page</Text>
-          <Text>
-            Random: video, stamps, animated character singin + buttons play
-            again & results
-          </Text>
+          <Video
+            ref={video}
+            style={styles.video}
+            source={require('../../assets/videos/piou.mp4')}
+            useNativeControls
+            resizeMode={ResizeMode.CONTAIN}
+            isLooping
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+          />
+          {/* <View style={styles.buttons}>
+            <Pressable
+              onPress={() =>
+                status.isPlaying
+                  ? video.current.pauseAsync()
+                  : video.current.playAsync()
+              }
+            >
+              <Text>{status.isPlaying ? 'Pause' : 'Play'}</Text>
+            </Pressable>
+          </View> */}
         </View>
       </View>
       <View className='flex flex-wrap space-x-10 flex-row mr-0 ml-auto'>
@@ -37,10 +58,19 @@ export default Reward;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'space-between', // Space exercises evenly
     flexWrap: 'wrap', // Allow wrapping into multiple rows if needed
+  },
+  video: {
+    alignSelf: 'center',
+    width: 640,
+    height: 400,
+    position: 'relative',
+    borderRadius: '50%',
   },
 });
