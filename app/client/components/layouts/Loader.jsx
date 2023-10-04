e423401bd020bc55d3cebbff366ba6789a6664ba
rjeantet <rose.jeantet@gmail.com>
2023-10-05 00:23:24 +0200

    Revert "Loader (two alternatives)"
    This reverts commit 40c2366a1698a6716901960b9b81bb39fa2c61cd.

--- app/client/components/layouts/Loader.jsx
+++ app/client/components/layouts/Loader.jsx
@@ -1,63 +1,11 @@
 import LayoutHOC from './LayoutHOC';
-import { StyleSheet, Text, View } from 'react-native';
-import Lottie from 'react-lottie';
-import loadingAnimation from '../../assets/animations/loading';
-import bearAnimation from '../../assets/animations/dancingbear';
-
-// Options for the animation
-const loadingOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: loadingAnimation,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-const bearOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: bearAnimation,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
+import { Text } from 'react-native';
 
 const Loader = () => {
-  //TODO: Add loader animation, center vertically content add background color, and style text
   return (
     <>
-      <View className='container flex flex-col items-center justify-center h-full bg-white'>
-        <View className='content-center'>
-          <Lottie
-            options={loadingOptions}
-            speed={30}
-            height={220}
-            width={220}
-            style={styles.loading}
-          />
-          <Lottie
-            options={bearOptions}
-            speed={30}
-            height={220}
-            width={220}
-            style={styles.loading}
-          />
-          <Text className='items-center'>Loading...</Text>
-        </View>
-      </View>
+      <Text>Loading...</Text>
     </>
   );
 };
 export default LayoutHOC(Loader);
-
-const styles = StyleSheet.create({
-  loading: {
-    textAlign: 'center',
-    justifyContent: 'center',
-    // position: 'absolute',
-    // left: '0px',
-    // top: '-50px',
-    // width: '35%',
-    // height: '35%',
-  },
-});
