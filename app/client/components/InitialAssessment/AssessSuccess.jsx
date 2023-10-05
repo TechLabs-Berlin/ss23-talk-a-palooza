ace7372ed523930c5340c22136ade5e6016a5f25
rjeantet <rose.jeantet@gmail.com>
2023-10-05 10:03:35 +0200

    Categories in initial assessment

--- app/client/components/InitialAssessment/AssessSuccess.jsx
+++ app/client/components/InitialAssessment/AssessSuccess.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { Link, Navigate } from 'react-router-dom';
 
 const AssessSuccess = ({ child, spokenWords }) => {
@@ -10,9 +10,9 @@ const AssessSuccess = ({ child, spokenWords }) => {
     <View>
       <Text>Thank you! We are looking forward to know {child.firstName}</Text>
       <Link to='/practice'>
-        <Button
+        <Pressable
           title='Start Exercises'
-          onClick={handleSubmit}
+          onPress={handleSubmit}
           spokenWords={spokenWords}
         />
       </Link>
