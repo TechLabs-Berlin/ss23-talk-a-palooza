b844dc09e028e4de00a88088df0d94a9b3ebcbe4
alj-b <aljoscha_b@icloud.com>
2023-10-15 01:53:13 +0200

    Install librosa

--- app/fastapi/dl_api/routers/audio.py
+++ app/fastapi/dl_api/routers/audio.py
@@ -3,12 +3,53 @@ from fastapi import APIRouter
 from typing import Dict, Tuple
 from fastai.vision.all import *
 from fastcore.utils import gt
+import numpy as np
+import matplotlib as plt
 import base64
 
 router = APIRouter()
 
 path = Path("./models")
 learn_inf = load_learner(path/'export.pkl')
+
+def convert_to_spectrogram(af, dst):   
+    x, sr = librosa.load(af, sr=44100)
+    X = librosa.stft(x)
+    Xdb = librosa.amplitude_to_db(abs(X))
+
+    plt.figure(frameon=False)
+    plt.axis('off')
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log', cmap='magma')
+
+    fname = af.split('/')[-1][:-4]
+    dst = dst + "/" + fname + "_doubled"
+    # print(dst)
+
+    plt.savefig(dst, dpi='figure', bbox_inches='tight',transparent=True, pad_inches=0)
+    plt.close()
+    
+def create_intelligibility_score_lax(prediction, target_word):
+  is_recognised = False
+  score = 0.0
+
+  ind = np.argpartition(prediction[2], -5)[-5:]
+  if prediction[1] in ind:
+    new_ind = np.delete(ind, [i for i in range(len(ind)) if ind[i] == prediction[1]])
+    if prediction[0] == target_word:
+      is_recognised = True
+      score = prediction[2][prediction[1]] - prediction[2][new_ind][np.argmax(prediction[2][new_ind])]
+      if score < 0.5: # for cases where the score would dip below .5, should be handled more elegantly
+        score = prediction[2][prediction[1]]
+    else:
+      diff = (prediction[2][prediction[1]] - prediction[2][new_ind][np.argmax(prediction[2][new_ind])])
+      if diff < 0.2 and prediction[2][prediction[1]] > 0.4:
+        is_recognised = True
+        score = prediction[2][prediction[1]]
+      else:
+        score = 0.5 - diff
+      if score < 0:
+        score = 0
+  return is_recognised, score
 
 class SingleQuery(BaseModel): 
     binaryAudioData: str # replace with binary audio data
@@ -35,7 +76,7 @@ async def rate_audio(q: SingleQuery): # declaring it as a required parameter
     with open("audio", 'w') as file:
         file.write(audio)
     
-    
+
     learn_inf.predict()
     response = SingleResponse(is_recognized = match, intelligibilityScore = score)
     return response
