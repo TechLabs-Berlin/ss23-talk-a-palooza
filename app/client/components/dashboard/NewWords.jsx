36ee7b80bc249bfe123cfdcfe1d23704e5cb6d8a
rjeantet <rose.jeantet@gmail.com>
2023-10-15 11:26:55 +0200

    Refactoring Typos: DashboardHeadings

--- app/client/components/dashboard/NewWords.jsx
+++ app/client/components/dashboard/NewWords.jsx
@@ -1,17 +1,14 @@
 import { Text, View } from 'react-native';
-import { Heading } from '../layouts/typo';
+import { DashboardHeading, Heading } from '../layouts/typo';
 
 function NewWords() {
   return (
     <>
-      <Text className='mb-2 text-lg font-semibold text-primary-dark '>
-        New Words
-      </Text>
+      <DashboardHeading text='New Words' />
       <Text className='mb-1 text-xs font-semibold uppercase text-slate-400 dark:text-slate-500'>
         Since last week
       </Text>
       <View className='flex flex-row items-start'>
-        <Text className='mr-2 text-3xl font-bold text-primary-dark '>6</Text>
         <Heading
           text={'6'}
           style={{
